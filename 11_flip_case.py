bc70c8b6eb71396c98fdcf48729f01474a23eac2
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-08-22 12:17:20 -0700

    two completed problems

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -11,3 +11,26 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+# need a result string
+# loop through string
+# compare letter to to_swap -> convert both to lowercase
+    # if same
+        # if letter to upper case == letter
+            #add it to string but make it lowercase
+        #if letter to lowercase == letter
+            # add it to string but make it uppercase
+    # if letter not the same add it to string
+# return the string
+    result = ""
+    for letter in phrase:
+        if letter.lower() == to_swap.lower():
+            # if letter.upper() == letter:
+            #     result += letter.lower()
+            # elif letter.lower() == letter:
+            #     result += letter.upper()
+            result += letter.lower() if letter.upper() == letter else letter.upper()
+        else:
+            result += letter
+    return result
+#think about "simpler solution"
+#nested ternary??
